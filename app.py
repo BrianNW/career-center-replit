3a253fb9884e2fd1f7b9deb5a73b4394c9092569
Brian <briantechdom@gmail.com>
2023-03-11 01:47:54 +0000

    Add jobs as a python list, passed it as hardcode

--- app.py
+++ app.py
@@ -5,9 +5,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Vancouver',
+    'salary': '60000'
+  },
+    {
+    'id': 2,
+    'title': 'Data Analyst2',
+    'location': 'Vancouver',
+    'salary': '70000'
+  },
+    {
+    'id': 3,
+    'title': 'Data Analyst3',
+    'location': 'Vancouver',
+    'salary': '80000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
