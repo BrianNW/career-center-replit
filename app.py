185f9a5df0edf8bde6219e5a5481c783530ea7f4
Brian <briantechdom@gmail.com>
2023-03-18 18:26:57 +0000

    Add new /jobs page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import modules from class
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 # using app, an object of the class Flask
 app = Flask(__name__)
@@ -35,6 +35,9 @@ JOBS = [
 def hello_world():
   return render_template('home.html', jobs=JOBS, company_name='Career Center')
 
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
